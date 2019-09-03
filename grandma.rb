80ca5650a9e6d52612950ef586b1eebd2355dac3
KG ZZ <czheng5h88@gmail.com>
2019-09-03 05:51:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,4 +14,10 @@
 
 def speak_to_grandma(argument)
   if argument.upcase != argument
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif argument.upcase == argument
+    return "NO, NOT SINCE 1938!"
+  elsif argument == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN"
+  end
 end
